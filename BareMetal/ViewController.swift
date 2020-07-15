e88a9fa13142cbf0107e53c0157cc24931084e05
Max Harris <harris.max@gmail.com>
2020-07-15 11:36:36 -0700

    add 2nd triangle, make both wiggle a bit on each frame

--- BareMetal/ViewController.swift
+++ BareMetal/ViewController.swift
@@ -10,17 +10,24 @@ import Metal
 import QuartzCore
 import UIKit
 
+public extension Float {
+    static func r(n: Float, tol: Float) -> Float {
+        let low = n - tol
+        let high = n + tol
+        return tol == 0 || low > high ? n : Float.random(in: low ..< high)
+    }
+}
+
 class ViewController: UIViewController {
     var device: MTLDevice!
     var metalLayer: CAMetalLayer
-    let vertexData: [Float] = [0.0, 0.5, 0.0,
-                               -0.5, -0.5, 0.0,
-                               0.5, -0.5, 0.0]
     var vertexBuffer: MTLBuffer!
     var vertexColorBuffer: MTLBuffer!
     var commandQueue: MTLCommandQueue!
     var renderPipelineState: MTLRenderPipelineState!
     var timer: CADisplayLink!
+
+    var vertexCount: Int = 0
 
     required init?(coder aDecoder: NSCoder) {
         metalLayer = CAMetalLayer()
@@ -39,11 +46,6 @@ class ViewController: UIViewController {
         metalLayer.framebufferOnly = true
         metalLayer.frame = view.layer.frame
         view.layer.addSublayer(metalLayer)
-
-        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
-        vertexBuffer = device.makeBuffer(bytes: vertexData,
-                                         length: dataSize,
-                                         options: .storageModeShared)
 
         guard let defaultLibrary = device.makeDefaultLibrary() else { return }
 
@@ -64,6 +66,43 @@ class ViewController: UIViewController {
         timer.add(to: RunLoop.main, forMode: RunLoop.Mode.default)
     }
 
+    func generateVerts () {
+        let tol: Float = Float.random(in: -0.1 ..< 0.1)
+
+        let vertexData: [Float] = [
+            Float.r(n: 0.0, tol: tol),
+            Float.r(n: 0.5, tol: tol),
+            Float.r(n: 0.0, tol: 0),
+
+            Float.r(n: -0.5, tol: tol),
+            Float.r(n: -0.5, tol: tol),
+            Float.r(n: 0.0, tol: 0),
+
+            Float.r(n: 0.5, tol: tol),
+            Float.r(n: -0.5, tol: tol),
+            Float.r(n: 0.0, tol: 0),
+
+            Float.r(n: -0.9, tol: tol),
+            Float.r(n: 0.1, tol: tol),
+            Float.r(n: 0.0, tol: 0),
+
+            Float.r(n: -0.1, tol: tol),
+            Float.r(n: -0.1, tol: tol),
+            Float.r(n:  0.0, tol: 0),
+
+            Float.r(n: -1.0, tol: tol),
+            Float.r(n: -1.0, tol: tol),
+            Float.r(n: 0.0, tol: 0)
+        ]
+
+        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
+        vertexBuffer = device.makeBuffer(bytes: vertexData,
+                                         length: dataSize,
+                                         options: .storageModeShared)
+
+        vertexCount = vertexData.count
+    }
+
     func render() {
         guard let drawable: CAMetalDrawable = metalLayer.nextDrawable() else { return }
 
@@ -72,11 +111,13 @@ class ViewController: UIViewController {
         renderPassDescriptor.colorAttachments[0].loadAction = .clear
         renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 255.0 / 255.0, green: 16.0 / 255.0, blue: 22.0 / 255.0, alpha: 1.0)
 
+        generateVerts()
+
         guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
         guard let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
         renderCommandEncoder.setRenderPipelineState(renderPipelineState)
         renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        renderCommandEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3)
+        renderCommandEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount)
         renderCommandEncoder.endEncoding()
         commandBuffer.present(drawable)
         // NB: you can pass in a time to present the finished image:
