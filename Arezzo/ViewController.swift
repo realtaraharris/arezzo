d9075fd619f86a869e2a2edc445ee9be769aeb41
Max Harris <harris.max@gmail.com>
2021-04-22 02:28:58 -0400

    move playback code into view controller
    it doesn't make sense to keep this hidden in a separate file now that it's not in a separate thread

--- Arezzo/ViewController.swift
+++ Arezzo/ViewController.swift
@@ -87,6 +87,97 @@ class ViewController: UIViewController, ToolbarDelegate {
         view.addSubview(self.toolbar.view) // add this last so that it appears on top of the metal layer
     }
 
+    func playback(runNumber: Int) {
+        guard self.drawOperationCollector.timestamps.count > 0 else {
+            return
+        }
+
+        check(AudioQueueNewOutput(&audioFormat, outputCallback, &self.playingState, CFRunLoopGetCurrent(), CFRunLoopMode.commonModes.rawValue, 0, &self.queue))
+
+        var buffers: [AudioQueueBufferRef?] = Array<AudioQueueBufferRef?>.init(repeating: nil, count: BUFFER_COUNT)
+
+        self.playingState.running = true
+
+        for i in 0 ..< BUFFER_COUNT {
+            check(AudioQueueAllocateBuffer(self.queue!, UInt32(bufferByteSize), &buffers[i]))
+            outputCallback(inUserData: &self.playingState, inAQ: self.queue!, inBuffer: buffers[i]!)
+
+            if !self.playingState.running {
+                break
+            }
+        }
+
+        let (startIndex, endIndex) = self.drawOperationCollector.getTimestampIndices(startPosition: self.startPosition, endPosition: self.endPosition)
+        var timestampIterator = self.drawOperationCollector.getTimestampIterator(startIndex: startIndex, endIndex: endIndex)
+
+        let recordedCursor = self.drawOperationCollector.timestamps[startIndex]
+        let recordedStart = self.drawOperationCollector.timestamps[0]
+        let timeOffset = recordedCursor - recordedStart
+
+        self.playingState.lastIndexRead = calcBufferOffset(timeOffset: timeOffset)
+
+        guard timestampIterator.count > 0 else { return }
+
+        let timeStart = self.drawOperationCollector.timestamps[0]
+        let timeEnd = self.drawOperationCollector.timestamps[self.drawOperationCollector.timestamps.count - 1]
+
+        let timeDelta = timeEnd - timeStart
+
+        let (firstTime, _) = timestampIterator.next()!
+        let playbackStart = CFAbsoluteTimeGetCurrent()
+
+        /*
+                   0         a   a'       b
+          recorded |---------|===,========|---------|
+          playback |---------------------------------------|===,========|---------|
+                   0                                       c   c'
+
+          timeDelta = b - a
+          currentPct = (c'-c + a'-a)/timeDelta
+
+          where
+            c' = playbackCursor
+            c = playbackStart
+            a' = recordedCursor
+            a = recordedStart
+         */
+
+        func renderNext(_: CFRunLoopTimer?) {
+            let playbackCursor = CFAbsoluteTimeGetCurrent()
+            let position: Float = Float((playbackCursor - playbackStart + recordedCursor - recordedStart) / timeDelta)
+
+            self.toolbar.playbackVC.playbackSlider.setValueEx(value: position)
+
+            if !self.playing {
+                return
+            }
+            let (currentTime, nextTime) = timestampIterator.next()!
+
+            if nextTime == -1 {
+                self.toolbar.playbackVC.playbackSlider.setValueEx(value: 1.0)
+                self.playingState.running = false
+                return
+            }
+
+            if runNumber < self.currentRunNumber { return }
+            self.currentRunNumber = runNumber
+
+            self.render(endTimestamp: currentTime)
+
+            let fireDate = playbackStart + nextTime - firstTime
+
+            self.nextRenderTimer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, 0, 0, 0, renderNext)
+            RunLoop.current.add(self.nextRenderTimer!, forMode: .common)
+        }
+
+        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, playbackStart, 0, 0, 0, renderNext)
+        RunLoop.current.add(timer!, forMode: .common)
+
+        check(AudioQueueStart(self.queue!, nil))
+
+        CFRunLoopRunInMode(CFRunLoopMode.defaultMode, BUFFER_DURATION, false)
+    }
+
     // MARK: - input event handlers
 
     override open func touchesBegan(_ touches: Set<UITouch>, with _: UIEvent?) {
